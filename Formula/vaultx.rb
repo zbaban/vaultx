8bd2574dc1d17657f938edc0261ce2b877343053
zbaban <zaidbaban@gmail.com>
2024-09-12 13:16:58 -0500

    Update sha256 value

--- Formula/vaultx.rb
+++ Formula/vaultx.rb
@@ -2,7 +2,7 @@ class Vaultx < Formula
   desc "Comprehensive Vault Cluster Status Checker"
   homepage "https://github.com/zbaban/vaultx"
   url "https://github.com/zbaban/vaultx/archive/v1.0.0.tar.gz"
-  sha256 "replace_with_actual_sha256_after_first_release"
+  sha256 "088ddfb3e395a2a987425153193d0ed0d1c77abe5cf64595bed1cd8bf3c96be5"
   license "MIT"
 
   depends_on "vault"
